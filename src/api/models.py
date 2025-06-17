282f5a72bb3c4d34e8b086ae313431a9817d1b30
Valentina López Vega <37059478+ValenLopezVega@users.noreply.github.com>
2025-06-17 17:27:46 +0000

    make user model

--- src/api/models.py
+++ src/api/models.py
@@ -6,14 +6,16 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    email: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    lastname: Mapped[str] = mapped_column(String(80), nullable=True)
+    password: Mapped[str] = mapped_column(String(200), nullable=False)
+    salt: Mapped[str] = mapped_column(String(80), nullable=False, default=1)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "lastname": self.lastname
             # do not serialize the password, its a security breach
         }
